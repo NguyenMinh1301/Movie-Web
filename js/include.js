eb8314ec663af81d0d16622e654d7a1ed3d88af1
NguyenMinh1301 <nguyenminhjqka@gmail.com>
2025-04-08 03:59:21 +0700

    Add include for Header and Footer

---
+++ js/include.js
@@ -0,0 +1,14 @@
+function includeHTML() {
+    const includes = document.querySelectorAll('[data-include]');
+    includes.forEach(el => {
+      const file = el.getAttribute('data-include');
+      fetch(file)
+        .then(res => res.text())
+        .then(data => {
+          el.innerHTML = data;
+        });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', includeHTML);
+  
